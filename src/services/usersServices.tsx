c7991f93d481fd7689e481ad36ddb23c69bb437f
Lionel Dabee <dabee.lionel@gmail.com>
2021-02-27 17:20:50 +0100

    test

--- src/services/usersServices.tsx
+++ src/services/usersServices.tsx
@@ -4,6 +4,7 @@ export const REACT_APP_ConnectAPI = process.env.REACT_APP_ConnectAPI;
 
 export async function UsersServ_RetrieveAllUsers(): Promise<IUser[] | undefined> {
   const allUsers: IUser[] | undefined = await axios
+    //test
     .get(`/users/allUsers`).then(responseArr => {
       const users: IUser[] = responseArr.data;
       return users;
